39a01ac01e16039713fa3a0d798c02cc154f7fff
somil24 <somil2402@gmail.com>
2021-10-01 10:52:39 +0530

    Created a folder for javascript and added hello world file

---
+++ Javascript/HelloWorld.js
@@ -0,0 +1,5 @@
+//Step 1 install Code runner extension
+//Step 2 To run the code Ctrl+Alt+N
+//Step 3 To stop the code Ctrl+Alt+M
+console.log("Hello World");
+console.log("Learn JavaScript it is a really useful language")
