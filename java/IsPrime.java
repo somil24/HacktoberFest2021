021132fc992b1f6c069f37936164f11daab68bb8
somil24 <somil2402@gmail.com>
2021-10-01 10:58:10 +0530

    Corrected the issues in Java program

--- java/IsPrime.java
+++ java/IsPrime.java
@@ -1,12 +1,21 @@
-public class isPrime(){
-public static boolean isPrime(int number){
-  int cont = 2;
-  boolean prime=true;
-  while ((prime) && (cont!=number)){
-    if (number % cont == 0)
-      prime = false;
-    cont++;
+import java.util.*;
+
+class IsPrime {
+  public static void main(String[] args) {
+    int a[] = { 1, 4, 5, 2, 6, 345, 435 };
+    for (int i : a)
+      System.out.println((isprime(i)));
+
   }
-  return prime;
-}
+
+  public static boolean isprime(int number) {
+    int cont = 2;
+    boolean prime = true;
+    while ((prime) && (cont != number)) {
+      if (number % cont == 0)
+        prime = false;
+      cont++;
+    }
+    return prime;
+  }
 }
